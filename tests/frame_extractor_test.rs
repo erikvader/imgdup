5d86eb7ae0fd26b5c01670759e56b68b1db8cca9
Erik Rimskog <erik.rimskog@gmail.com>
2023-07-18 15:53:45 +0200

    frame extractor tests on generated video

--- tests/frame_extractor_test.rs
+++ tests/frame_extractor_test.rs
@@ -1,35 +1,57 @@
-use std::{path::PathBuf, process::Stdio};
+use std::{path::PathBuf, process::Stdio, time::Duration};
+
+use imgdup::frame_extractor::{self, FrameExtractor};
+
+const TEST_VIDEO_FRAMES: usize = 250;
+const TEST_VIDEO_LENGTH_SEC: u64 = 10;
 
 #[test]
-fn test_seeking_at_eof() {
+fn test_seeking_at_eof() -> frame_extractor::Result<()> {
     let video = create_test_video();
-    // sök till slutet
-    // hämta frames tills det inte går längre
-    // sök framåt
-    // kolla att det fortfarande är Ok(None)
+    let mut frames = FrameExtractor::new(video)?;
+
+    frames.seek_forward(Duration::from_secs(TEST_VIDEO_LENGTH_SEC - 1))?;
+    assert!(frames.iter().all(|frame| frame.is_ok()));
+
+    frames.seek_forward(Duration::from_secs(1))?;
+    assert!(matches!(frames.next(), Ok(None)));
+    Ok(())
+}
+
+#[test]
+fn test_total_frames() -> frame_extractor::Result<()> {
+    let video = create_test_video();
+    let num_frames = FrameExtractor::new(video)?
+        .iter()
+        .filter(|res| res.is_ok())
+        .take(TEST_VIDEO_FRAMES + 1)
+        .count();
+    assert_eq!(TEST_VIDEO_FRAMES, num_frames);
+    Ok(())
 }
 
 fn create_test_video() -> PathBuf {
     let tmpdir =
         PathBuf::from(option_env!("CARGO_TARGET_TMPDIR").unwrap()).join("testvideo.mkv");
 
-    if tmpdir.exists() {
-        return tmpdir;
-    }
-
-    std::process::Command::new("ffmpeg")
-        .args([
-            "-f",
-            "lavfi",
-            "-i",
-            "testsrc=duration=10:size=1280x720:rate=30",
-            tmpdir.as_os_str().to_str().expect("no probs, probably"),
-        ])
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .stdin(Stdio::null())
-        .status()
-        .expect("failed to execute ffmpeg");
+    use std::sync::Once;
+    static ONCE: Once = Once::new();
+    ONCE.call_once(|| {
+        std::fs::remove_file(&tmpdir).ok();
+        std::process::Command::new("ffmpeg")
+            .args([
+                "-f",
+                "lavfi",
+                "-i",
+                "testsrc=duration=10:rate=25",
+                tmpdir.as_os_str().to_str().expect("no probs, probably"),
+            ])
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .stdin(Stdio::null())
+            .status()
+            .expect("failed to execute ffmpeg");
+    });
 
     tmpdir
 }
